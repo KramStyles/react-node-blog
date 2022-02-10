90c50b5a64788c2571c70789d9b800d16e2187e8
kramstyles <mark.eke@decagon.dev>
2022-02-10 12:38:33 +0100

    added substring

--- src/Components/ArticlePage.js
+++ src/Components/ArticlePage.js
@@ -7,10 +7,15 @@ const ArticlePage = () => (
         <div className="container">
             <h2>This is the Article Page</h2>
             <a href="/list">Article List With Reload</a> <br/>
-            <Link to="/list">Article List Without Reload</Link>
+            <Link to="/list">Article List Without Reload</Link> <br/>
 
             {Data.map(item => (
-                <Link to={'/list/'+item.country}><br/>{item.country}</Link>
+                // <Link to={'/list/'+item.country}><br/>{item.country}</Link>
+                <Link to={`/list/${item.country}`}>
+                    <br/>
+                    <h3>{item.country}</h3>
+                    <p>{item.description.substring(0, 100)} ...</p>
+                </Link>
             ))}
         </div>
     </>
