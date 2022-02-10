c40da000d2ea8bff056ee61e93b8e32af6afb2f9
kramstyles <mark.eke@decagon.dev>
2022-02-10 13:46:16 +0100

    fixed 404

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 
@@ -20,6 +20,7 @@ function App() {
         <Route path="about" element={<AboutPage />} />
         <Route path="article" element={<ArticlePage />} />
         <Route path="list/:endpoint" element={<ArticleList />} />
+        <Route path = "*" element={<NotFound />}/>
       </Routes>
       <NavBar />
     </Router>
